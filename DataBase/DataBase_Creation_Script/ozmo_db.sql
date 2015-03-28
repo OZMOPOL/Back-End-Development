e1c054a64abd5b141d038f21bb4d8a2cac14ea3d
atharkh <atharkh@gmail.com>
2015-03-28 18:37:24 +0200

    Updates of database for webservice calls

--- DataBase/DataBase_Creation_Script/ozmo_db.sql
+++ DataBase/DataBase_Creation_Script/ozmo_db.sql
@@ -12,21 +12,19 @@ DROP TABLE IF EXISTS Room;
 DROP TRIGGER IF EXISTS make_post_c_time;
 
 /* # Let there be Rooms */
-/* Status will be shown as 1 for "ACTIVE" and 0 for "INACTIVE"  */
-
+/* Status will be shown as "1" for "ACTIVE" and "0" for "INACTIVE"  */
 CREATE TABLE Room
 (
 pk_room_id varchar(32) UNIQUE NOT NULL,
 room_title varchar(64) NOT NULL UNIQUE,
 room_desc text,
-room_status bit NOT NULL DEFAULT 1,
+room_status varchar(32) NOT NULL,
 
 PRIMARY KEY (pk_room_id)
 );
 
 /* Let there be Users */
-/* Status will be shown as 1 for "ACTIVE" and 0 for "INACTIVE"  */
-
+/* Status will be shown as "1" for "ACTIVE" and "0" for "INACTIVE"  */
 CREATE TABLE User
 (
 pk_user_id varchar(32)  UNIQUE NOT NULL,
@@ -34,13 +32,12 @@ user_name varchar(32) NOT NULL UNIQUE,
 user_pass char(64) NOT NULL,
 user_email varchar(32) NOT NULL UNIQUE,
 user_bday date,
-user_status bit NOT NULL DEFAULT 0,
+user_status varchar(32) NOT NULL,
 
 PRIMARY KEY (pk_user_id)
 );
 
 /* Let there be Posts */
-
 CREATE TABLE Post
 (
 pk_post_id varchar(32) UNIQUE NOT NULL,
@@ -48,8 +45,6 @@ post_title varchar(256),
 post_content text NOT NULL,
 post_c_date DATETIME ,
 post_e_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-post_status  bit NOT NULL DEFAULT 1,
-
 fk_post_user_id varchar(32) NOT NULL,
 fk_post_room_id varchar(32) NOT NULL,
 fk_post_prnt_id varchar(32),
@@ -62,7 +57,6 @@ FOREIGN KEY (fk_post_prnt_id) REFERENCES Post(pk_post_id)
 );
 
 /* Let There be Votes */
-
 CREATE TABLE Vote
 (
 pk_vote_id varchar(32) UNIQUE NOT NULL,
@@ -79,7 +73,6 @@ UNIQUE KEY user_vote_post (fk_vote_user_id, fk_vote_post_id)
 );
 
 /* Let Users Follow Rooms */
-
 CREATE TABLE X_user_flw_room
 (
 pk_userXroom_id varchar(32) UNIQUE NOT NULL,
@@ -95,7 +88,6 @@ UNIQUE KEY user_flw_room (fk_userXroom_user_id,fk_userXroom_room_id)
 );
 
 /* Let Users Follow Users */
-
 CREATE TABLE X_user_flw_user
 (
 pk_userXuser_id varchar(32) UNIQUE NOT NULL,
@@ -113,7 +105,6 @@ UNIQUE KEY user_flw_user (fk_userXuser_flwr_user_id, fk_userXuser_flwd_user_id)
 
 
 /* When post is edited, post_e_time is updated */
-
 DELIMITER //
 
 CREATE TRIGGER make_post_c_time 
@@ -127,12 +118,15 @@ END //
 
 DELIMITER ;
 
+
+
 /*** And the time came to pass ***/
+
+/*...*/
 
 /*** Day #2: LET THERE BE TUPLES IN TABLES ACCORDINGLY ***/
 
 /* Then Rooms had tuples... */
-
 INSERT INTO Room VALUES
 ('ffffffffffffffff', 'HALL', 'HALL','1'),
 ('3uh2gkejasdy9243', 'Dining Hall', 'Wahz cookin doc?','1'),
@@ -145,7 +139,6 @@ INSERT INTO Room VALUES
 
 
 /* So did Users... */
-
 INSERT INTO User VALUES
 ('ffffffffffffffff', 'GOD', 'ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff', 'god@ozu.edu.tr', '1000-01-01','1'),
 ('bjkweshgk48y93s4', 'Mosey', 'bjkweshgk48y93s4bjkweshgk48y93s4bjkweshgk48y93s4bjkweshgk48y93s4', 'mosey@ozu.edu.tr', '1100-01-01','1'),
@@ -176,11 +169,6 @@ DO SLEEP(1);
 
 INSERT INTO Post VALUES
 ('hhegdteuiwoodadr', NULL, 'anytime after 8 is fine... before 6 in the next morning ofcourse (6_6) ', NULL , NULL ,'euritolfkjnndjfj', 'rrhysd276185jstf', 'lrncndhrkktnchw0' );
-
-DO SLEEP(1);
-
-INSERT INTO Post VALUES
-('hhegdteuiwooda22', NULL, 'Ill be late tonite ', NULL , NULL ,'euritolfkjnndjfj', 'rrhysd276185jstf', 'lrncndhrkktnchw0' );
 
 DO SLEEP(1);
 
@@ -224,7 +212,6 @@ INSERT INTO Vote VALUES ('yuiuyuiuyuiuyuiu', '1', 'euritolfkjnndjfj', 'krt74yeha
 INSERT INTO Vote VALUES ('tqwftqwftqwftqwf', '-1', 'urb49ne9bek30mme', 'owieowieowieowie');
 INSERT INTO Vote VALUES ('frgtfrgtfrgtfrgt', '-1', 'euritolfkjnndjfj', 'owieowieowieowie');
 INSERT INTO Vote VALUES ('41234rgtfrg33rgt', '-1', 'ffffffffffffffff', 'polishnfnfojndkn');
-
 /* People also could change her/his opinion.... Don't you think different when you learn something new?? */
 
 UPDATE Vote SET vote_value='-1' WHERE pk_vote_id='ykdrykdrykdrykdr';
@@ -236,17 +223,16 @@ UPDATE Vote SET vote_value='1' WHERE pk_vote_id='frgtfrgtfrgtfrgt';
 
 /* So everyone can follow Rooms  */
 
-INSERT INTO X_user_flw_room VALUES('trtgtrtgtrtgtrtg', 'euritolfkjnndjfj','3uh2gkejasdy9243');
-INSERT INTO X_user_flw_room VALUES('reteretereterete', 'geidgeidgeidgeid', 'rakhsd2775632g4s');
-INSERT INTO X_user_flw_room VALUES('retyretyretyrety', 'bjkweshgk48y93s4', 'uiybewg4jh3g4gbs');
-INSERT INTO X_user_flw_room VALUES('efrtefrtefrtefrt', 'ffffffffffffffff', 'uiybewg4jh3g4gbs');
+INSERT INTO X_user_flw_room VALUES('trtgtrtgtrtgtrtg','euritolfkjnndjfj','3uh2gkejasdy9243');
+INSERT INTO X_user_flw_room VALUES('reteretereterete' , 'geidgeidgeidgeid' , 'rakhsd2775632g4s');
+INSERT INTO X_user_flw_room VALUES('retyretyretyrety', 'bjkweshgk48y93s4' , 'uiybewg4jh3g4gbs');
+INSERT INTO X_user_flw_room VALUES('efrtefrtefrtefrt', 'ffffffffffffffff' , 'uiybewg4jh3g4gbs');
 
 /* And people can follow other people as well, Intresting!! */
-
 INSERT INTO X_user_flw_user VALUES ('gnvbgnvbgnv33nvb', 'urb49ne9bek30mme', 'bjksefkhjw49ub43');
 INSERT INTO X_user_flw_user VALUES ('iopliopliopliopl', 'bjksefkhjw49ub43', 'geidgeidgeidgeid');
-INSERT INTO X_user_flw_user VALUES ('rthdrthdrthdrthd', 'euritolfkjnndjfj', 'ffffffffffffffff');
-INSERT INTO X_user_flw_user VALUES ('gnvbgnvbgnvbgnvb', 'geidgeidgeidgeid', 'bjksefkhjw49ub43');
-INSERT INTO X_user_flw_user VALUES ('jnmvjnmvjnmvjnmv', 'urb49ne9bek30mme', 'euritolfkjnndjfj');
-INSERT INTO X_user_flw_user VALUES ('iopdiopdiopdiopd', 'urb49ne9bek30mme', 'geidgeidgeidgeid');
-INSERT INTO X_user_flw_user VALUES ('dfrtdfrtdfrtdfrt', 'ffffffffffffffff', 'euritolfkjnndjfj');
+INSERT INTO X_user_flw_user VALUES ('rthdrthdrthdrthd', 'euritolfkjnndjfj' , 'ffffffffffffffff');
+INSERT INTO X_user_flw_user VALUES ('gnvbgnvbgnvbgnvb', 'geidgeidgeidgeid' , 'bjksefkhjw49ub43');
+INSERT INTO X_user_flw_user VALUES ('jnmvjnmvjnmvjnmv', 'urb49ne9bek30mme' , 'euritolfkjnndjfj');
+INSERT INTO X_user_flw_user VALUES ('iopdiopdiopdiopd', 'urb49ne9bek30mme' , 'geidgeidgeidgeid');
+INSERT INTO X_user_flw_user VALUES ('dfrtdfrtdfrtdfrt', 'ffffffffffffffff' , 'euritolfkjnndjfj');
