06d3eef1736762d0104d67e4b8ca47a8979fa765
solsav <sol.1917@gmail.com>
2015-04-11 22:36:15 +0300

    Added custom classes and services for: ozRoom ozResult ozPost ozUser

---
+++ ozmoPol_WS_GF3/src/java/OzClass/OzUser.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package OzClass;
+
+import com.ozmo.ent.User;
+
+/**
+ *
+ * @author amind
+ */
+public class OzUser extends User{
+    
+}
