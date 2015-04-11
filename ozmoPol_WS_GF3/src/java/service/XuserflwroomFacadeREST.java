cc69c45d8a6c96610c895a053e26d53e14efb250
solsav <sol.1917@gmail.com>
2015-04-11 20:11:17 +0300

    GlassFish 3 deployment AUTOMATED TEST

---
+++ ozmoPol_WS_GF3/src/java/service/XuserflwroomFacadeREST.java
@@ -0,0 +1,88 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package service;
+
+import com.ozmo.ent.Xuserflwroom;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+/**
+ *
+ * @author sav
+ */
+@Stateless
+@Path("com.ozmo.ent.xuserflwroom")
+public class XuserflwroomFacadeREST extends AbstractFacade<Xuserflwroom> {
+    @PersistenceContext(unitName = "ozmoPol_WS_GF3PU")
+    private EntityManager em;
+
+    public XuserflwroomFacadeREST() {
+        super(Xuserflwroom.class);
+    }
+
+    @POST
+    @Override
+    @Consumes({"application/xml", "application/json"})
+    public void create(Xuserflwroom entity) {
+        super.create(entity);
+    }
+
+    @PUT
+    @Override
+    @Consumes({"application/xml", "application/json"})
+    public void edit(Xuserflwroom entity) {
+        super.edit(entity);
+    }
+
+    @DELETE
+    @Path("{id}")
+    public void remove(@PathParam("id") String id) {
+        super.remove(super.find(id));
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces({"application/xml", "application/json"})
+    public Xuserflwroom find(@PathParam("id") String id) {
+        return super.find(id);
+    }
+
+    @GET
+    @Override
+    @Produces({"application/xml", "application/json"})
+    public List<Xuserflwroom> findAll() {
+        return super.findAll();
+    }
+
+    @GET
+    @Path("{from}/{to}")
+    @Produces({"application/xml", "application/json"})
+    public List<Xuserflwroom> findRange(@PathParam("from") Integer from, @PathParam("to") Integer to) {
+        return super.findRange(new int[]{from, to});
+    }
+
+    @GET
+    @Path("count")
+    @Produces("text/plain")
+    public String countREST() {
+        return String.valueOf(super.count());
+    }
+
+    @java.lang.Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+    
+}
